0353feb27cba48c10fa46f391b90fe562970c4dc
Locki Lo <locki.lo@cargosmart.com>
2018-11-19 17:08:01 +0800

    Completed

--- main/main.js
+++ main/main.js
@@ -1,9 +1,26 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(in1,in2) {
+	return in1%in2;
+};
 
-function caclSum() {};
+function caclSum(array) {
+	var sum = array.reduce(getSum, 0);
+	return sum;
+};
 
-function caclSumInConditon() {};
+function getSum(total, num){
+	return total + num;
+};
+
+function caclSumInConditon(array, num) {
+	var result = 0;
+	for(var i=0; i<array.length; i++){
+		if(array[i] < num){
+			result += array[i];
+		}
+	}
+	return result;
+};
 
 module.exports = {
     caclRemaindar,
